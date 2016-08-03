e5c0b89b823d142960a781807ba44e93e12c2f66
SharonHart <dmastersharon@gmail.com>
2016-08-04 08:09:06 +0900

    added main rotation loop,

--- parse_map.py
+++ parse_map.py
@@ -1,34 +1,57 @@
 from TEMPy.MapParser import *
 from TEMPy.ScoringFunctions import *
-import scipy.signal as sps
+from TEMPy.StructureBlurrer import *
+from scipy.ndimage.filters import gaussian_filter
+from scipy.signal import correlate
+import numpy as np
 
 input_path = "./source/emd_1094.map"
 cylinder_path = "./source/Cylinder.mrc"
 target_map = MapParser.readMRC(input_path)
 cylinder_map = MapParser.readMRC(cylinder_path)
 
-# get small matrix of
-nparr = cylinder_map.fullMap
-nparr = nparr[50:57, 47:54, 47:54] # shape: 7X7X7
+# trim cylinder to it smallest form
+cylinder_array = cylinder_map.fullMap
+cylinder_array = cylinder_array[50:57, 47:54, 47:54] # shape: 7X7X7
 
-a = sps.correlate(target_map.fullMap, nparr, mode="valid")
+# smooth cylinder by gaussian mask TODO: figure out how to get sigma
+sigma = target_map.apix
+cylinder_array = gaussian_filter(cylinder_array, sigma)
+
+cylinder_map.fullMap = cylinder_array
+
+max_scores = np.zeros(target_map.box_size())
+max_dirs = np.zeros(target_map.box_size(), dtype=(float,2))
 
 
-print(a.shape)
-print(a.max())
+sanity = 0
+rad_in_x = 0 # accumulate rotation in the x axis (in rads)
+for i in range (12): #for every x rotation
+    rad_in_x += (math.pi/12) * i
+    rad_in_y = 0 #accumulate rotation in the y axis (in rads)
+    for j in range (12): #for every y rotation
+        rad_in_y += (math.pi/12) * j
+        template_cylinder = cylinder_map
+        template_cylinder.rotate_by_axis_angle(x=1, y=0, z=0, angle=rad_in_x, CoM=cylinder_map.centre(), rad=True)
+        template_cylinder.rotate_by_axis_angle(x=0, y=1, z=0, angle=rad_in_y, CoM=cylinder_map.centre(), rad=True)
+        correlation_matrix = correlate(target_map.fullMap, template_cylinder.fullMap, mode="same")
+        # scipy correlate is super-slow :(
+        for (x,y,z), value in np.ndenumerate(correlation_matrix):
+            if value > max_scores[x,y,z]:
+                max_scores[x,y,z] = value
+                max_dirs[x,y,z] = (rad_in_x, rad_in_y)
 
-t = target_map.change_origin(0,0,0)
-print(t.origin)
-print(t.box_size())
-cylinder_map = cylinder_map.resize_map((60,60,60))
-print (target_map.box_size())
+        sanity += 1
+        print sanity
 
-s = ScoringFunctions()
+print max_scores
+print max_dirs
 
-score = s.CCC(cylinder_map, t)
 
-print ("score:{} ".format(score))
-print(t.origin)
+#score = s.CCC(cylinder_map, t)
+
+#print ("score:{} ".format(score))
+#print(t.origin)
 # print(c.origin)
 # print(t.box_size())
 # print(c.box_size())
