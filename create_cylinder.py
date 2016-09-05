8eefff97edf4bfca428e23b2ab878b11d91b492f
sharon <dmastersharon@gmail.com>
2016-09-06 01:10:39 +0300

    code orginazing

--- create_cylinder.py
+++ create_cylinder.py
@@ -4,32 +4,29 @@
 
 from TEMPy.MapParser import MapParser
 
-def main(target_path, output_path):
-
-
-    map_target = MapParser.readMRC(target_path)
+def main(target_map):
 
     # 2 Very very simple example of the cylinder
     dr = 2.3  # radius of the cylinder
     h = 10.8  # height of the cylinder
     rho = 1  # density of the cylinder
 
-    map_target.shift_origin(0, 0, 0)
+    target_map.shift_origin(0, 0, 0)
 
-    dz = len(map_target.fullMap) * map_target.apix / 2
-    dy = len(map_target.fullMap[0]) * map_target.apix / 2
-    dx = len(map_target.fullMap[0][0]) * map_target.apix / 2
+    dz = len(target_map.fullMap) * target_map.apix / 2
+    dy = len(target_map.fullMap[0]) * target_map.apix / 2
+    dx = len(target_map.fullMap[0][0]) * target_map.apix / 2
     # points_array = []
-    for z in range(len(map_target.fullMap)):
-        for y in range(len(map_target.fullMap[z])):
-            for x in range(len(map_target.fullMap[z][y])):
-                z_coor = z * map_target.apix - dz
-                y_coor = y * map_target.apix - dy
-                x_coor = x * map_target.apix - dx
+    for z in range(len(target_map.fullMap)):
+        for y in range(len(target_map.fullMap[z])):
+            for x in range(len(target_map.fullMap[z][y])):
+                z_coor = z * target_map.apix - dz
+                y_coor = y * target_map.apix - dy
+                x_coor = x * target_map.apix - dx
 
                 if (z_coor < h) & (z_coor > 0) & (y_coor * y_coor + x_coor * x_coor < dr * dr):
-                    map_target.fullMap[z][y][x] = rho
+                    target_map.fullMap[z][y][x] = rho
                 else:
-                    map_target.fullMap[z][y][x] = 0
+                    target_map.fullMap[z][y][x] = 0
 
-    map_target.write_to_MRC_file(output_path)
+    return target_map
